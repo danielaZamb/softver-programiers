4fb3c49e049ce7811e7f8ee5913c8f9407a31541
danielaZamb <91443941+danielaZamb@users.noreply.github.com>
2024-04-13 23:00:26 -0500

    se crea componente pagian inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalNavComponent } from './principal-nav/principal-nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalNavComponent,
-    FooterComponent
+    FooterComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
